ba9ff8a300db18e74cf6ea09ffe36139ea9f4e95
Axel Huerta <axelhuerta.dev@gmail.com>
2024-06-11 23:05:56 -0600

    update style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { useUeas } from "../store/Store";
 
 type Props = {
@@ -19,37 +18,35 @@ export default function Card(props: Readonly<Props>) {
 
   return (
     <div
-      className={`card w-80 sm:w-96 shadow-xl mx-auto my-4 ${
+      className={`card w-80 sm:w-96 shadow-xl my-4 ${
         approvedUeasID.includes(props.id)
           ? "bg-success text-black"
           : "bg-neutral"
       }`}
     >
       <div className="card-body">
-        <h2 className="card-title capitalize">{props.name}</h2>
-        <div className="grid grid-cols-3">
-          {props.type.includes("optativa") ? null : (
-            <>
-              <div className="font-extrabold">
-                <span className="text-gray-400">clave</span>
-                <p>{props.id}</p>
-              </div>
-              <div className="font-extrabold">
-                <span className="text-gray-400">créditos</span>
-                <p>{props.credits}</p>
-              </div>
-              <button
-                className={`btn ${
-                  approvedUeasID.includes(props.id)
-                    ? "btn-neutral text-white"
-                    : "btn-primary"
-                }`}
-              >
-                Detalles
-              </button>
-            </>
-          )}
+        {/* header */}
+        <div className="flex justify-between items-center font-extrabold">
+          <div>
+            <span className="text-gray-400">clave: </span>
+            {props.id}
+          </div>
+          <span>copy</span>
         </div>
+
+        {/* title */}
+        <h2 className="card-title capitalize h-[100px] justify-center">{props.name}</h2>
+        {props.type.includes("optativa") ? null : (
+          <div className="flex justify-between w-full px-2">
+            <div className="font-extrabold">
+              <span className="text-gray-400">créditos</span>
+              <p>{props.credits}</p>
+            </div>
+            <button className="btn btn-primary btn-outline text-right">
+              Aprobar
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
