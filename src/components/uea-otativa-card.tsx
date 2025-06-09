66f7b4032f515d815a5694ae327f36cb95a285b4
Axel Huerta <axelhuerta.dev@gmail.com>
2025-06-09 13:11:44 -0600

    update optativa card

--- src/components/uea-otativa-card.tsx
+++ src/components/uea-otativa-card.tsx
@@ -1,4 +1,4 @@
-import { CircleFadingPlus } from "lucide-react";
+import { CircleFadingPlus, Edit } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -23,12 +23,20 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { useUeaStore } from "@/store/ueas-store";
 import { Badge } from "./ui/badge";
+import { useState } from "react";
 
 const formSchema = z.object({
   registerId: z.string().length(7, {
@@ -47,9 +55,19 @@ interface Props {
   name: string;
 }
 
+const statusValues = [
+  { value: "pending", label: "Pendiente" },
+  { value: "in-progress", label: "En curso" },
+  { value: "approved", label: "¡Aprobada!" },
+];
+
 export default function UeaOptativaCard({ id, name }: Props) {
   const ueasStore = useUeaStore((state) => state.ueas);
   const updateStatus = useUeaStore((state) => state.updateStatus);
+
+  const registeredUea = ueasStore.find((uea) => uea.id === id);
+
+  const [status, setStatus] = useState(getStatus());
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -60,12 +78,23 @@ export default function UeaOptativaCard({ id, name }: Props) {
     },
   });
 
-  function getRegisteredUea() {
-    const registeredUea = ueasStore.find((uea) => uea.id === id);
-    console.log("Registered UEA: ", registeredUea);
+  function handleSelectChange(value: string) {
+    updateStatus({
+      id,
+      status: value,
+      credits: registeredUea ? registeredUea.credits : 0,
+      register: {
+        id: registeredUea?.register ? registeredUea.register.id : "",
+        name: registeredUea?.register ? registeredUea?.register.name : "",
+      },
+    });
+
+    setStatus(value);
   }
 
-  getRegisteredUea();
+  function getStatus() {
+    return registeredUea ? registeredUea.status : "pending";
+  }
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     const newUea = {
@@ -94,94 +123,116 @@ export default function UeaOptativaCard({ id, name }: Props) {
             <Badge>{uea?.status}</Badge>
           </div>
         </CardHeader>
-        <CardContent className="flex justify-center">
-          {/* Dialog */}
-          <Dialog>
-            <DialogTrigger>
-              <CircleFadingPlus className="h-32 w-32 text-neutral-700 cursor-pointer" />
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle className="mb-4">
-                  Ingresa los datos de la UEA optativa
-                </DialogTitle>
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-8"
-                  >
-                    {/* name */}
-                    <FormField
-                      control={form.control}
-                      name="registerName"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>UEA</FormLabel>
-                          <FormControl>
-                            <Input
-                              placeholder="Ej. Temas Selectos de Ingeniería de Software"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormDescription>Nombre de la UEA</FormDescription>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-
-                    <div className="flex gap-4">
-                      {/* id */}
+        <CardContent>
+          <div className="flex justify-between items-center">
+            <p>
+              Créditos: <span>{uea?.credits}</span>
+            </p>
+            {/* Dialog */}
+            <Dialog>
+              <DialogTrigger>
+                <Edit className="h-6 w-6 cursor-pointer" />
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle className="mb-4">
+                    Ingresa los datos de la UEA optativa
+                  </DialogTitle>
+                  <Form {...form}>
+                    <form
+                      onSubmit={form.handleSubmit(onSubmit)}
+                      className="space-y-8"
+                    >
+                      {/* name */}
                       <FormField
                         control={form.control}
-                        name="registerId"
+                        name="registerName"
                         render={({ field }) => (
                           <FormItem>
-                            <FormLabel>Clave</FormLabel>
-                            <FormControl>
-                              <Input placeholder="Ej. 2151124" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                              Clave asociada a la UEA
-                            </FormDescription>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-
-                      {/* credits */}
-                      <FormField
-                        control={form.control}
-                        name="registerCredits"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Créditos</FormLabel>
+                            <FormLabel>UEA</FormLabel>
                             <FormControl>
                               <Input
-                                type="number"
-                                min={1}
-                                placeholder="Ej. 11"
+                                placeholder="Ej. Temas Selectos de Ingeniería de Software"
                                 {...field}
-                                onChange={(e) =>
-                                  field.onChange(e.target.valueAsNumber)
-                                }
                               />
                             </FormControl>
-                            <FormDescription>
-                              Créditos asociados a la UEA
-                            </FormDescription>
+                            <FormDescription>Nombre de la UEA</FormDescription>
                             <FormMessage />
                           </FormItem>
                         )}
                       />
-                    </div>
-                    <div className="flex justify-end">
-                      <Button type="submit">Registrar</Button>
-                    </div>
-                  </form>
-                </Form>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
+
+                      <div className="flex gap-4">
+                        {/* id */}
+                        <FormField
+                          control={form.control}
+                          name="registerId"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Clave</FormLabel>
+                              <FormControl>
+                                <Input placeholder="Ej. 2151124" {...field} />
+                              </FormControl>
+                              <FormDescription>
+                                Clave asociada a la UEA
+                              </FormDescription>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+
+                        {/* credits */}
+                        <FormField
+                          control={form.control}
+                          name="registerCredits"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Créditos</FormLabel>
+                              <FormControl>
+                                <Input
+                                  type="number"
+                                  min={1}
+                                  placeholder="Ej. 11"
+                                  {...field}
+                                  onChange={(e) =>
+                                    field.onChange(e.target.valueAsNumber)
+                                  }
+                                />
+                              </FormControl>
+                              <FormDescription>
+                                Créditos asociados a la UEA
+                              </FormDescription>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+                      <div className="flex justify-end">
+                        <Button type="submit">Registrar</Button>
+                      </div>
+                    </form>
+                  </Form>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </div>
+          <Select
+            onValueChange={(value) => {
+              handleSelectChange(value);
+            }}
+            defaultValue={status}
+          >
+            <SelectTrigger className="w-full my-4">
+              <SelectValue placeholder="Estado de la UEA" />
+            </SelectTrigger>
+            <SelectContent>
+              {statusValues.map((status) => (
+                <SelectItem key={status.value} value={status.value}>
+                  {status.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </CardContent>
       </Card>
     );
